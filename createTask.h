e668304979f86e644ea24f4bb9279b2f1f84c4dd
Lukasz <Lukasz.Szupka@student.fh-kiel.de>
2023-05-09 15:48:52 +0200

    createTask initialize a vector "task" and adds a new task to it.

---
+++ createTask.h
@@ -0,0 +1,16 @@
+//
+// Created by lukas on 09.05.2023.
+//
+
+#ifndef PIC_AEM_GRUPPE_6_CREATETASK_H
+#define PIC_AEM_GRUPPE_6_CREATETASK_H
+
+#include <string>
+#include <vector>
+
+class CreateTask {
+public:
+    static void newTask();
+};
+
+#endif //PIC_AEM_GRUPPE_6_CREATETASK_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "createTask.h"
 
 int main() {
+
+    CreateTask::newTask();
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
