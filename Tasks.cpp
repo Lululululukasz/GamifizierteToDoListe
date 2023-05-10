ad82accb6769eba3e0d1f199cc058542e0ff2042
Jan Ole <jan-weighardt@web.de>
2023-05-10 13:51:34 +0200

    feat: task Anzeigen

--- Tasks.cpp
+++
@@ -1,8 +0,0 @@
-#include "Tasks.h"
-
-namespace ToDoListAdventure {
-    void Tasks::newTask(std::string task) {
-        // Add new tasks to the vector
-        this->task.emplace_back(task);
-    };
-} // ToDoListAdventure
