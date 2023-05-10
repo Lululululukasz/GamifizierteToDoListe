67e7e154c065435ea2f323c27ca8dcb959e2eaff
Mika <mika.schick@student.fh-kiel.de>
2023-05-10 18:39:53 +0200

    feature: deleteTask finished and tested added: algorithm in header

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+
 #include "Task.h"
 
 using namespace ToDoListAdventure;
@@ -6,7 +6,9 @@ using namespace ToDoListAdventure;
 int main() {
     Tasks tasks = Tasks();
     tasks.newTask("test");
+    tasks.newTask("Dishes");
     tasks.showTask();
+    tasks.deleteTask();
     return 0;
 }
 
