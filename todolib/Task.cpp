2b47fa568bbe5c6a24d22c463d728e06a569a2e5
Helene Luening <helene.i.n.luening@student.fh-kiel.de>
2023-05-12 17:23:14 +0200

    task.cpp complete excluding datetimes

--- todolib/Task.cpp
+++ todolib/Task.cpp
@@ -3,7 +3,26 @@
 //
 
 #include "Task.h"
+#include <time.h>
+
+
+using namespace std;
+
 
 Task::Task(string n, string d) {
-    this->id = n;
+    this->id = n + to_string((int)time(NULL));
+    this->name = n;
+    this->description = d;
+}
+
+bool Task::getDoneStatus() {
+    return this->done;
+}
+
+void Task::setAsDone() {
+    this->done = true;
+}
+
+void Task::setAsUndone() {
+    this->done = false;
 }
