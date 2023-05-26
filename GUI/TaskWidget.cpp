5599026984a8fdfdf69729f51fcd18603ad3e723
vera <veronica.zylla@student.fh-kiel.de>
2023-05-26 13:38:06 +0200

    added comments to the code I wrote in sprint 1.

--- GUI/TaskWidget.cpp
+++ GUI/TaskWidget.cpp
@@ -11,34 +11,42 @@
 TaskWidget::TaskWidget(todolib::Task &task, QWidget *parent)
         : task{task}, QWidget(parent) {
 
+    //layouts
     hbox = std::make_shared<QHBoxLayout>();
     vbox = std::make_shared<QVBoxLayout>(this);
 
+    //taskCheckbox
     taskCheckbox = std::make_shared<QCheckBox>(this);
     taskCheckbox->setCheckState(Qt::Unchecked);
 
+    //taskNameLabel
     taskNameLabel = std::make_shared<QLabel>(this);
     taskNameLabel->setText(QString::fromStdString(task.name));
 
+    //taskDeleteButton
     taskDeleteButton = std::make_shared<QPushButton>();
     taskDeleteButton->setText("  Delete");
     taskDeleteButton->setIcon(taskDeleteButton->style()->standardIcon(QStyle::SP_TrashIcon));
 
+    //showDescriptionButton
     showDescriptionButton = std::make_shared<QToolButton>(this);
     showDescriptionButton->setCheckable(true);
     showDescriptionButton->setArrowType(Qt::RightArrow);
     showDescriptionButton->setStyleSheet("QToolButton{border: none;}");
 
+    //taskDescriptionLabel
     taskDescriptionLabel = std::make_shared<QLabel>();
     taskDescriptionLabel->setWordWrap(true);
     taskDescriptionLabel->setText(QString::fromStdString(task.description));
 
+    //adding widgets to the layouts
     vbox->addLayout(hbox.get());
     hbox->addWidget(taskCheckbox.get(), 0, Qt::AlignLeft | Qt::AlignVCenter);
     hbox->addWidget(showDescriptionButton.get(), 0, Qt::AlignLeft | Qt::AlignVCenter);
     hbox->addWidget(taskNameLabel.get(), 1, Qt::AlignLeft | Qt::AlignVCenter);
     hbox->addWidget(taskDeleteButton.get(), 0, Qt::AlignRight | Qt::AlignVCenter);
 
+    //connecting to the slots below
     connect(taskDeleteButton.get(), &QPushButton::clicked, this, &TaskWidget::deleteTask);
     connect(taskCheckbox.get(), &QCheckBox::stateChanged, this, &TaskWidget::strikeoutTask);
     connect(showDescriptionButton.get(), &QToolButton::toggled, [=,this](bool checked) {
@@ -49,6 +57,7 @@ TaskWidget::TaskWidget(todolib::Task &task, QWidget *parent)
 
 }
 
+//changes the state of the taskNameLabel to strikedout or not strikedout
 void TaskWidget::strikeoutTask(int state) {
 
     QFont *font = new QFont;
@@ -64,15 +73,18 @@ void TaskWidget::strikeoutTask(int state) {
     }
 }
 
+//emits the deleteTaskSignal that is used in CategoryWidget
 void TaskWidget::deleteTask() {
     emit deleteTaskSignal();
 }
 
+//shows the description of a task when the showDescriptionButton is checked
 void TaskWidget::showDescription() {
     taskDescriptionLabel->setVisible(true);
     vbox->addWidget(taskDescriptionLabel.get(), 0, Qt::AlignLeft | Qt::AlignVCenter);
 }
 
+//hides the description of a task when the showDescriptionButton is unchecked
 void TaskWidget::hideDescription() {
 
     hbox->removeWidget(taskDescriptionLabel.get());
