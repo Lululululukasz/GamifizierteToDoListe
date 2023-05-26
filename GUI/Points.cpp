254b84e0031da5d2888150f895a7109b9a428d33
Lars Funk <lars.funk@student.fh-kiel.de>
2023-05-26 16:45:47 +0200

    more changes to Points, checking tasks adds points

--- GUI/Points.cpp
+++ GUI/Points.cpp
@@ -1,7 +1,13 @@
 #include "Points.h"
 
-Points::Points(unsigned int np, unsigned int dp) : nPoints{np}, dPoints{dp}{}
+//Whith this one you can use the points class with "Points::getinstance()" its always the same instance.
+Points& Points::getinstance(){
+    static Points instance;
+    return instance;
+}
 
+//these two add and subtract points from the variables, you can add a multiplier variable if you want if not set it to 1
+//nORd decides if you want to change the normal or default points.
 void Points::addPoints(unsigned int newpoints, double multi, char nORd) {
     if(nORd == 'n'){
         this->nPoints += static_cast<unsigned int>(newpoints * multi);
@@ -10,13 +16,23 @@ void Points::addPoints(unsigned int newpoints, double multi, char nORd) {
     }
 }
 void Points::subPoints(unsigned int leavepoints, char nORd) {
-    if(nORd == 'n' && this->nPoints > 0){
-        this->nPoints -= leavepoints;
-    }else if(nORd == 'd' && this->dPoints > 0){
-        this->dPoints -= leavepoints;
+    if(nORd == 'n'){
+        if(this->nPoints >= leavepoints){
+            this->nPoints -= leavepoints;
+        }else{
+            this->nPoints = 0;
+        }
+    }else if(nORd == 'd'){
+        if(this->dPoints >= leavepoints){
+            this->dPoints -= leavepoints;
+        }else{
+            this->dPoints = 0;
+        }
     }
 }
 
-unsigned int * Points::getNPoints() {return &nPoints;}
-unsigned int * Points::getDPoints() {return &dPoints;}
+unsigned int Points::getNPoints() {return nPoints;}
+unsigned int Points::getDPoints() {return dPoints;}
 
+void Points::setNPoints(unsigned int newpoints) {nPoints = newpoints;}
+void Points::setDPoints(unsigned int newpoints) {dPoints = newpoints;}
--- GUI/Points.h
+++ GUI/Points.h
@@ -1,18 +1,26 @@
 #ifndef PIC_AEM_PROJEKT_POINTS_H
 #define PIC_AEM_PROJEKT_POINTS_H
 
-
-class Points {
+class Points{
+public:
 private:
-    Points(unsigned int np, unsigned int dp);
+    Points(){};
+
     unsigned int nPoints;
     unsigned int dPoints;
 public:
+    static Points& getinstance();
+    Points(Points const&) = delete;
+    void operator=(Points const&) = delete;
+
+    //Adds and sub
     void addPoints(unsigned int newpoints, double multi, char nORd);
     void subPoints(unsigned int leavepoints, char nORd);
 
-    unsigned int * getNPoints();
-    unsigned int * getDPoints();
+    unsigned int getNPoints();
+    unsigned int getDPoints();
+    void setNPoints(unsigned int newpoints);
+    void setDPoints(unsigned int newpoints);
 };
 
 
