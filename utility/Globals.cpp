e1e5bfa68c88c786300d6ed1fca21e22dac87ad9
Peter Prins <peter.prins@student.fh-kiel.de>
2023-05-27 19:05:45 +0200

    Comments and edit Installation guide

--- utility/Globals.cpp
+++ utility/Globals.cpp
@@ -2,13 +2,19 @@
 #include "Globals.h"
 #include <QtCore/QJsonArray>
 
-
+/**
+ * Function to shorten the currentPath depending on where the Program is started from.
+ * @return {homepath} the shortend currentPath
+ */
 QString changeHomepath() {
     QString homepath = QDir::currentPath();
+    // Path in the deployment, where the executable is in.
+    // This is the case, when the Executable is started directly in folder instead of the windows-menu
     int binTrue = homepath.indexOf("/bin", homepath.length() - 4);
     if (binTrue != -1) {
         homepath.remove(binTrue, 4);
     }
+    // Path in windows-debugging, where the build executable is started from, when no working directory is declared
     int debugDirectory = homepath.indexOf("/cmake-build-debug", homepath.length() - 18);
     if (debugDirectory != -1) {
         homepath.remove(debugDirectory, 18);
--- utility/Globals.h
+++ utility/Globals.h
@@ -1,7 +1,11 @@
 #ifndef PIC_AEM_PROJEKT_GLOBALS_H
 #define PIC_AEM_PROJEKT_GLOBALS_H
+#include <QString>
 QString changeHomepath();
 
+/**
+ * Class for all global static variables
+ */
 class globals{
 public:
     const static inline QString homepath = changeHomepath();
